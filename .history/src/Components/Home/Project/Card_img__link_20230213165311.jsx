1df39d87884726b84856e5a50f095470b962da8a
khalid <dev.khalidhossain@gmail.com>
2023-02-13 17:02:27 +0600

    new style adding

---
+++ .history/src/Components/Home/Project/Card_img__link_20230213165311.jsx
@@ -0,0 +1,17 @@
+// import githubIcon from "../../../Assests/icons/github.png"
+// import redirect from "../../../Assests/icons/redirect.svg"
+import "./Projects.css"
+export default function Card_img__link(props) {
+  
+  return (
+     <div className="card_img__link">
+              <a href={props.link } target="_blank" rel="noreferrer" >
+               <i class="fa-brands fa-github-alt"></i>
+              </a>
+              <a href={props.preview} target="_blank" rel="noreferrer" >
+                  <i class="fa-solid fa-arrow-up-right-from-square redirect"></i>
+              </a>
+               
+    </div>
+  )
+}
--- .history/src/Components/Home/Skill/SkillSet_20230213164643.jsx
+++ .history/src/Components/Home/Skill/SkillSet_20230213164643.jsx
@@ -32,32 +32,32 @@ export default function SkillSet() {
                             <li><img src={bsIcon} alt="Bootstrap icon"/>Bootstrap</li> 
                             <li><img src={gitIcon} alt="Git & GitHub icon"/>Git & GitHub</li>
                             <li><img src={apiIcon} alt="RestAPI icon"/>RestAPI</li>
-                            <li><img src={npmIcon} alt="npm icon"/>npm</li>
-                            <li><img src={eslintIcon} alt="Eslint icon"/>Eslint</li>
+                            <li><img src={npmIcon} alt="icon"/>npm</li>
+                            <li><img src={eslintIcon} alt="icon"/>Eslint</li>
                     </div>
 
                     <div className="skillSet col-md-3">
                         <div className="skillSet__title__text">Also Worked with</div>
-                            <li><img src={firebaseIcon} alt="Firebase icon"/>Firebase</li>
-                            <li><img src={nodejsIcon} alt="Nodejs icon"/>Nodejs</li>
-                            <li><img src={expressjsIcon} alt="ExpressJs icon"/>ExpressJs</li>
-                            <li><img src={webpackIcon} alt="Webpack icon"/>Webpack</li> 
-                            <li><img src={reduxIcon} alt="Redux icon"/>Redux</li>
+                            <li><img src={firebaseIcon} alt="icon"/>Firebase</li>
+                            <li><img src={nodejsIcon} alt="icon"/>Nodejs</li>
+                            <li><img src={expressjsIcon} alt="icon"/>ExpressJs</li>
+                            <li><img src={webpackIcon} alt="icon"/>Webpack</li> 
+                            <li><img src={reduxIcon} alt="icon"/>Redux</li>
                     </div>
                     
                     <div className="skillSet col-md-3">
                          <div className="skillSet__title__text">My Services</div>
-                            <li><img src={webIcon} alt="Responsive Web Design"/>Responsive Web Design</li>
-                            <li><img src={codeFileIcon} alt="Pixel Perfect Design"/>Pixel Perfect Design</li>
-                            <li><img src={codeIcon} alt="Modern Web App Development"/>Modern Web App Development</li>
+                            <li><img src={webIcon} alt="icon"/>Responsive Web Design</li>
+                            <li><img src={codeFileIcon} alt="icon"/>Pixel Perfect Design</li>
+                            <li><img src={codeIcon} alt="icon"/>Modern Web App Development</li>
                     </div>
                         
                     <div className="skillSet col-md-3">
                         <div className="skillSet__title__text">Curently Learning</div>
-                            <li><img src={reduxIcon} alt="Redux icon"/>Redux</li>
-                            <li><img src={firebaseIcon} alt="Firebase icon"/>Firebase</li>
-                            <li><img src={typescriptIcon} alt="TypeScript icon"/>TypeScript</li>
-                            <li><img src={nodejsIcon} alt="NodeJs icon"/>NodeJs</li>
+                            <li><img src={reduxIcon} alt="icon"/>Redux</li>
+                            <li><img src={firebaseIcon} alt="icon"/>Firebase</li>
+                            <li><img src={typescriptIcon} alt="icon"/>TypeScript</li>
+                            <li><img src={nodejsIcon} alt="icon"/>NodeJs</li>
                     </div>
                     
                     </ul>
